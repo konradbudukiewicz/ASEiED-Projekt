d3178dd9b2fbd6e2bb31e4e4898fcf293c4a00f1
Maciej Baniukiewicz <39955040+baniukiewicz@users.noreply.github.com>
2021-09-19 17:10:11 +0200

    Update install_python_modules.sh

--- install_python_modules.sh
+++ install_python_modules.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
-sudo pip3 install boto3
-sudo pip3 install rasterio
-
-
+sudo pip3 install boto3==1.18.43
+sudo pip3 install rasterio==1.2.8
+sudo pip3 install affine==2.3.0
+sudo pip3 install attrs==21.2.0
+sudo pip3 install aws-cfn-bootstrap==2.0
+sudo pip3 install beautifulsoup4==4.9.3
+sudo pip3 install boto==2.49.0
+sudo pip3 install botocore==1.21.43
+sudo pip3 install certifi==2021.5.30
+sudo pip3 install click==8.0.1
+sudo pip3 install click-plugins==1.1.1
+sudo pip3 install cligj==0.7.2
+sudo pip3 install docutils==0.14
+sudo pip3 install importlib-metadata==4.8.1
+sudo pip3 install jmespath==0.10.0
+sudo pip3 install joblib==1.0.1
+sudo pip3 install lockfile==0.11.0
+sudo pip3 install lxml==4.6.3
+sudo pip3 install mysqlclient==1.4.2
+sudo pip3 install nltk==3.6.2
+sudo pip3 install nose==1.3.4
+sudo pip3 install numpy==1.16.5
+sudo pip3 install py-dateutil==2.2
+sudo pip3 install pyparsing==2.4.7
+sudo pip3 install pystache==0.5.4
+sudo pip3 install python-daemon==2.2.3
+sudo pip3 install python-dateutil==2.8.2
+sudo pip3 install python37-sagemaker-pyspark==1.4.1
+sudo pip3 install pytz==2021.1
+sudo pip3 install PyYAML==5.4.1
+sudo pip3 install regex==2021.8.3
+sudo pip3 install s3transfer==0.5.0
+sudo pip3 install simplejson==3.2.0
+sudo pip3 install six==1.13.0
+sudo pip3 install snuggs==1.4.7
+sudo pip3 install tqdm==4.62.1
+sudo pip3 install typing-extensions==3.10.0.2
+sudo pip3 install urllib3==1.26.6
+sudo pip3 install windmill==1.6
+sudo pip3 install zipp==3.5.0
   
